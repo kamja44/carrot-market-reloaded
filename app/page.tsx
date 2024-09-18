6fcf1ea8765d2a1091434798366afb99bf0c00cd
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-09-18 20:24:46 +0900

    Directives

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 h-screen    flex items-center 2xl:bg-purple-100 justify-center p-5 ">
+    <main className="bg-red-400 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 h-screen    flex items-center 2xl:bg-purple-100 justify-center p-5 ">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3">
         <div className="group flex flex-col">
           <input
@@ -10,9 +10,9 @@ export default function Home() {
           <span className="group-focus-within:block hidden">
             Make sure it is a valid email...
           </span>
-          <button className="w-full h-[345.178px] bg-[#543cb8] text-white rounded-kamja m-tomato">
-            Submit
-          </button>
+          <a href="#">aaaa</a>
+          <button className="btn">Submit</button>
+          <input className="btn" placeholder="layer사용" />
         </div>
       </div>
     </main>
