f73f8aa743f764ff67763ccaafd809ad4a070121
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-09-20 01:53:15 +0900

    Server Actions

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,12 @@
-"use client";
-
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocalLogin from "@/components/social-login";
 
 export default function LogIn() {
-  const onClick = async () => {
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({
-        username: "kamja",
-        password: "1234",
-      }),
-    });
-    console.log(await response.json());
+  const handleForm = async (formData: FormData) => {
+    "use server";
+    console.log(formData.get("email"), formData.get("password"));
+    console.log("i run in the server only!!");
   };
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
@@ -21,18 +14,23 @@ export default function LogIn() {
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with Email and Password.</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[""]} />
+      <form action={handleForm} className="flex flex-col gap-3">
         <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={[""]}
+        />
+        <FormInput
+          name="password"
           type="password"
           placeholder="Password"
           required
           errors={[""]}
         />
-      </form>
-      <span onClick={onClick}>
         <FormButton loading={false} text="Log in" />
-      </span>
+      </form>
       <SocalLogin />
     </div>
   );
