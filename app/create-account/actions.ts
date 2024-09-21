df1eab3cd4e73b2ab93afd87f9f07f79f0f98536
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-09-22 00:46:52 +0900

    transformation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,6 +1,10 @@
 "use server";
 import { z } from "zod";
 
+const passwordRegex = new RegExp(
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/
+);
+
 const checkUsername = (username: string) => !username.includes("potato");
 const checkPasswords = ({
   password,
@@ -19,10 +23,18 @@ const formSchema = z
       })
       .min(3, "이름이 너무 짧아요 !")
       .max(10, "이름이 너무 길어요 !")
+      .toLowerCase()
+      .trim()
       .refine(checkUsername, "No potatoes allowed"),
-    email: z.string().email(),
-    password: z.string().min(10),
-    confirmPassword: z.string().min(10),
+    email: z.string().email().toLowerCase(),
+    password: z
+      .string()
+      .min(4)
+      .regex(
+        passwordRegex,
+        "A password must have lowercase, UPPERCASE, a number and special characters"
+      ),
+    confirmPassword: z.string().min(4),
   })
   .refine(checkPasswords, {
     message: "Both passwords should be the same",
@@ -41,5 +53,7 @@ export async function createAccount(prevState: any, formData: FormData) {
   if (!result.success) {
     console.log(result.error.flatten());
     return result.error.flatten();
+  } else {
+    console.log(result.data);
   }
 }
