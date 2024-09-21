e85daca3beb72336906134c1df4483e5c35bd09c
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-09-21 16:59:07 +0900

    useFormState

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,13 @@
+"use client";
+
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocalLogin from "@/components/social-login";
+import { useFormState } from "react-dom";
+import { handleForm } from "./actions";
 
 export default function LogIn() {
-  const handleForm = async (formData: FormData) => {
-    "use server";
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    console.log("logged in!");
-  };
+  const [state, action] = useFormState(handleForm, null); // action은 handleForm을 실행하기 위한 trigger이다.
 
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
@@ -15,20 +15,20 @@ export default function LogIn() {
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with Email and Password.</h2>
       </div>
-      <form action={handleForm} className="flex flex-col gap-3">
+      <form action={action} className="flex flex-col gap-3">
         <FormInput
           name="email"
           type="email"
           placeholder="Email"
           required
-          errors={[""]}
+          errors={[]}
         />
         <FormInput
           name="password"
           type="password"
           placeholder="Password"
           required
-          errors={[""]}
+          errors={state?.errors ?? []}
         />
         <FormButton text="Log in" />
       </form>
