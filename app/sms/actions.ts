9e2ed832db21aaf1c31bf35f7e5f62aceb7df7b0
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-09-23 03:00:21 +0900

    SMS Validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -2,12 +2,50 @@
 
 import { z } from "zod";
 import validator from "validator";
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+import { redirect } from "next/navigation";
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "Wrong phone Format"
+  );
 
 // coerce => user가 입력한 string을 number로 변환한다.
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsVerification(prevState: any, formData: FormData) {
-  console.log(typeof formData.get("token"));
-  console.log(typeof tokenSchema.parse(formData.get("token")));
+interface ActionState {
+  token: boolean;
+}
+
+export async function smsVerification(
+  prevState: ActionState,
+  formData: FormData
+) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      console.log(result.error.flatten());
+      return {
+        token: false,
+        error: result.error.flatten(),
+      };
+    } else {
+      return {
+        token: true,
+      };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      return {
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      redirect("/");
+    }
+  }
 }
