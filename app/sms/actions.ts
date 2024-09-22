6dba43fea5f3f3a08a40729776f3b531e5842a5c
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-09-22 17:14:51 +0900

    Coerce 강제 변환

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,3 +1,13 @@
-"user server";
+"use server";
 
-export async function smsVerification(prevState: any, formData: FormData) {}
+import { z } from "zod";
+import validator from "validator";
+const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+
+// coerce => user가 입력한 string을 number로 변환한다.
+const tokenSchema = z.coerce.number().min(100000).max(999999);
+
+export async function smsVerification(prevState: any, formData: FormData) {
+  console.log(typeof formData.get("token"));
+  console.log(typeof tokenSchema.parse(formData.get("token")));
+}
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -14,12 +14,14 @@ export default function SMSLogin() {
         <h2 className="text-xl">VErify your phone number.</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input name="phone" type="number" placeholder="Phone number" required />
+        <Input name="phone" type="text" placeholder="Phone number" required />
         <Input
           name="token"
           type="number"
           placeholder="Verification code"
           required
+          min={100000}
+          max={999999}
         />
         <Button text="Verify" />
       </form>
