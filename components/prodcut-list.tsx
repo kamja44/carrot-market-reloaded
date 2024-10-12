2b7989a6ca9ad69d9dbd533e304d4a29b29d5279
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-10-12 22:14:35 +0900

    Infinite Scrolling with IntersectionObserver

--- components/prodcut-list.tsx
+++ components/prodcut-list.tsx
@@ -2,7 +2,7 @@
 
 import { InitialProducts } from "@/app/(tabs)/products/page";
 import ListProduct from "./list-products";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getMoreProducts } from "@/app/(tabs)/products/action";
 
 interface ProductListProps {
@@ -20,24 +20,60 @@ interface ProductListProps {
 export default function ProductList({ initialProducts }: ProductListProps) {
   const [products, setProducts] = useState(initialProducts);
   const [isLoading, setIsLoading] = useState(false);
-  const onLoadMoreClick = async () => {
-    setIsLoading(true);
-    const newProducts = await getMoreProducts(1);
-    setProducts((prev) => [...prev, ...newProducts]);
-    setIsLoading(false);
-  };
+  const [page, setPage] = useState(0);
+  const [isLastPage, setIsLastPage] = useState(false);
+  const trigger = useRef<HTMLSpanElement>(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      async (
+        entries: IntersectionObserverEntry[],
+        observer: IntersectionObserver
+      ) => {
+        const element = entries[0];
+        if (element.isIntersecting && trigger.current) {
+          observer.unobserve(trigger.current);
+          setIsLoading(true);
+          const newProducts = await getMoreProducts(page + 1);
+          if (newProducts.length !== 0) {
+            setPage((prev) => prev + 1);
+            setProducts((prev) => [...prev, ...newProducts]);
+          } else {
+            setIsLastPage(true);
+          }
+          setIsLoading(false);
+        }
+      },
+      {
+        threshold: 1.0,
+        rootMargin: "0px 0px -100px 0px",
+      }
+    );
+    if (trigger.current) {
+      observer.observe(trigger.current);
+    }
+    // 아래의 return 문은 cleanup funciton이다.
+    // user가 페이지를 떠날 떄 시랳오딘다.
+    // 즉, component가 unmount될 때 실행된다.
+    return () => {
+      observer.disconnect();
+    };
+  }, [page]);
   return (
     <div className="p-5 flex flex-col gap-5">
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      <button
-        onClick={onLoadMoreClick}
-        disabled={isLoading}
-        className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
-      >
-        {isLoading ? "로딩 중" : "Load more"}
-      </button>
+      {!isLastPage ? (
+        <span
+          ref={trigger}
+          style={{
+            marginTop: `${(page + 1) * 900}vh`,
+          }}
+          className=" mb-96 text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+        >
+          {isLoading ? "로딩 중" : "Load more"}
+        </span>
+      ) : null}
     </div>
   );
 }
